494a8851faa1e30eed914fa32dafe1e1b5999d37
jangrubin <wkdfnqls2465@gmail.com>
2023-03-10 16:56:17 +0900

    스타일링은 forEach문을 익혀야 할 것 같다

--- source/README.md
+++ source/README.md
@@ -20,3 +20,69 @@ issue-2
 
 issue-3
 예준씨의 도움으로 내가 원하던 것을 만들 수 있었다 ㅜㅜ 매개변수, 객체를 활용하는 법을 좀 더 연습해야 할 것 같다
+-> \_EXAMDATA의 데이터를 html 문서내에 모두 출력하는데 원하는 section에 넣고 싶다
+->설계 안하고 전체제어 함수부터 만들다가 머리 폭발
+
+issue-4
+현재 원하는 위치에 스타일을 적용하고 싶은데 for문돌려서 하는게 맞나..?
+디비전 부터 생성하고 작업해야함
+아니 뭔가만 손대면 스타일도 여기저기 다 적용가능한데 해답을 몰그ㅔㅆ다...
+
+onst \_EXAMDATA = {
+teamInformation: {
+teamNumber: 5,
+teamName: "newDevs",
+teamNameStory: "뉴진스의 하입보이요.",
+teamManager: "박준형",
+// 프로젝트에 참가한 팀원 명단입니다.
+teamAllMember: [
+{
+memberNumber: 0,
+name: "준형",
+teamPosition: "팀장",
+githubAddress: "https://www.github.com/",
+},
+{
+memberNumber: 1,
+name: "윤호",
+teamPosition: "에이스",
+githubAddress: "https://www.github.com/",
+},
+{
+memberNumber: 2,
+name: "예준",
+teamPosition: "연구원",
+githubAddress: "https://www.github.com/",
+},
+{
+memberNumber: 3,
+name: "해경",
+teamPosition: "홍일점",
+githubAddress: "https://www.github.com/",
+},
+{
+memberNumber: 4,
+name: "루빈",
+teamPosition: "졸개",
+githubAddress: "https://www.github.com/",
+},
+],
+},
+trainingInformation: {
+subject: [
+"웹프로그래밍기본(HTML5&CSS3)",
+"Git&GitHub",
+"Babel Javascript",
+"프레임워크 기초",
+"Project C(선택)",
+"백엔드프레임워크런타임",
+"프레임워크활용",
+"Project A(데이터 기반 주식 추천 서비스)",
+"Project B(교통정보제공서비스)",
+],
+},
+designInformation: {
+colorSet: ["cadetblue", "salmon", "#ccc", "#333"],
+breakPoint: ["768px", "1280px", "1920px"],
+},
+};
--- source/modulefile.js
+++ source/modulefile.js
@@ -14,31 +14,31 @@ const _EXAMDATA = {
         memberNumber: 0,
         name: "준형",
         teamPosition: "팀장",
-        githubAddress: "https://www.github.com/",
+        githubAddress: "https://www.github.com/joon",
       },
       {
         memberNumber: 1,
         name: "윤호",
         teamPosition: "에이스",
-        githubAddress: "https://www.github.com/",
+        githubAddress: "https://www.github.com/yoon",
       },
       {
         memberNumber: 2,
         name: "예준",
         teamPosition: "연구원",
-        githubAddress: "https://www.github.com/",
+        githubAddress: "https://www.github.com/yeah",
       },
       {
         memberNumber: 3,
         name: "해경",
         teamPosition: "홍일점",
-        githubAddress: "https://www.github.com/",
+        githubAddress: "https://www.github.com/hye",
       },
       {
         memberNumber: 4,
         name: "루빈",
         teamPosition: "졸개",
-        githubAddress: "https://www.github.com/",
+        githubAddress: "https://www.github.com/roo",
       },
     ],
   },
@@ -77,7 +77,7 @@ const _EXAMDATA = {
 const pageMaker = {
   tagName: ["div"],
   innerContent: ["생성"],
-  Value: ["id"],
+  Value: ["id", "class"],
   valueName: ["header", "main", "footer"],
   parent: [root],
 
@@ -104,11 +104,25 @@ const pageMaker = {
   //   // console.log("hi");
   // },
   // 디비전 생성 -> where에 어디의 자식으로 할건지 경로지정, a에 몇개 만들건지 씀
-  div: function (where, a) {
+  div: function (where, a, textWhat) {
     for (let i = 0; i < a; i++) {
       let ele = document.createElement("div");
-      ele.textContent = this.innerContent[0];
+      // 사진이나 텍스트
+      // ele.setAttribute(this.Value[1], className);
+      ele.innerHTML = textWhat;
+      // 원하는 위치에 요소 생성
       where.appendChild(ele);
+    }
+  },
+  // 클래스 적용하고 내용 설정 가능
+
+  // 출력해야 할 개인 내용이 깃허브주소, 넘버, 이름, 포지션,
+  innerDiv: function (innerWhere, s, setText) {
+    for (let i = 0; i < s; i++) {
+      let innerEle = document.createElement("div");
+      innerEle.setAttribute("class", [i] + "class");
+      innerEle.innerHTML = setText;
+      innerWhere.appendChild(innerEle);
     }
   },
 };
@@ -124,22 +138,49 @@ const pageMaker = {
 
 // 헤더 메인 푸터 생성
 pageMaker.bodyWrap();
-// header에 디비전 생성
-pageMaker.div(root.children[0], 1);
+// header에 팀 네임>로고 생성
+pageMaker.div(root.children[0], 1, _EXAMDATA["teamInformation"].teamName);
+
+// pageMaker.div()
 // pageMaker.styler("root");
 // 헤더 자식으로 div 1개 생성
 // pageMaker.teamInfo(1);
+
 // 메인
-pageMaker.div(root.children[1], 3);
+pageMaker.div(
+  // 위치
+  root.children[1],
+  // 몇개 만들지
+  _EXAMDATA.teamInformation.teamAllMember.length,
+  // 들어갈 텍스트 요소
+  "팀 인원수만큼 디비전 생성"
+);
+// const teamInfoHeader = root.children[1].querySelector();
+console.log(root.children[1]);
+// pageMaker.div(roo)
+// n번째 main div 접근
+
+// 팀원의 개인정보
+console.log(_EXAMDATA.teamInformation.teamAllMember[0]);
+
+for (let i = 0; i < _EXAMDATA.teamInformation.teamAllMember.length; i++) {
+  pageMaker.innerDiv(
+    root.children[1].children[i],
+    4,
+    // 팀원 개인의 깃허브 주소
+    _EXAMDATA.teamInformation.teamAllMember[i].githubAddress
+  );
+}
 
 // 푸터
+pageMaker.div(root.children[2], 1);
 
 function teamIntro() {
   for (let i = 0; i < a; i++) {}
 }
-const teamIntroduce = document.createElement("div");
-teamIntroduce.textContent = _EXAMDATA["teamInformation"].teamName;
-root.children[0].appendChild(teamIntroduce);
+// const teamIntroduce = document.createElement("div");
+// teamIntroduce.textContent = _EXAMDATA["teamInformation"].teamName;
+// root.children[0].appendChild(teamIntroduce);
 // 랜덤 팀원 이름
 // console.dir(_EXAMDATA["teamInformation"].teamAllMember[i].name);
 console.dir(_EXAMDATA["teamInformation"].teamAllMember[0].teamPosition);
@@ -188,9 +229,12 @@ console.dir(_EXAMDATA["teamInformation"].teamAllMember[0].teamPosition);
 // // root.style[styleWorld.Color[0]] = styleValue[3];
 
 const styleBox = {
-  // 각 스타일별로 나누기엔 너무 어려움 ㅠ
+  // 각 스타일 적용할 위치
+  styleWhere: [root, root.children[0]],
+  // 스타일 요소들
   StyleSelect: [
     // size
+    "margin",
     "width",
     "height",
     // color
@@ -205,14 +249,50 @@ const styleBox = {
     "alignItems",
   ],
   // 스타일 적용할 변수 선언하고 데이터 넣어주면 됨
-  headerStyleValue: ["100vw", "10vh", "green", "yellow"],
-  mainStyleValue: ["100vw", "70vh", "yellow"],
-  footerStyleValue: [,],
-  styler: function (styleWhere) {
+  rootStyleValue: ["0", "100vw", "100vh", "green", "yellow"],
+  headerStyleValue: ["0", "100vw", "10vh", "pink"],
+  mainStyleValue: ["0", "100vw", "70vh", "yellow"],
+  footerStyleValue: ["0", "100vw", "20vh", "black"],
+  InfoBoxStyleValue: [
+    "0",
+    "300px",
+    "500px",
+    "white",
+    "black",
+    "20px",
+    "",
+    "flex",
+    "column",
+    "center",
+    "center",
+  ],
+  // #root 스타일
+
+  rootStyler: function () {
     for (let i = 0; i < this.StyleSelect.length; i++) {
-      styleWhere.style[this.StyleSelect[i]] = this.headerStyleValue[i];
+      this.styleWhere[0].style[this.StyleSelect[i]] = this.rootStyleValue[i];
+    }
+  },
+  // 헤더 스타일
+  headerStyler: function () {
+    for (let i = 0; i < this.StyleSelect.length; i++) {
+      this.styleWhere[1].style[this.StyleSelect[i]] = [
+        this.headerStyleValue[i],
+      ];
+    }
+  },
+  // 외부에서 핸들링해야하는 -> 값이 자주 바뀌는 친구들은 매개변수로 써주는게 좋고 , 우리가 계속 사용할 -> 바뀌지 않을 애들을 함수로 쓰는게 맞다 ㅜㅠㅜ
+  commonStyler: function (comsty, choiceStyle) {
+    for (let i = 0; i < this.StyleSelect.length; i++) {
+      comsty.style[this.StyleSelect[i]] = choiceStyle[i];
     }
   },
 };
+
 // console.log(root.children[0].style["width"]);
-styleBox.styler(root.children[0]);
+styleBox.rootStyler();
+console.log(root.children[0]);
+styleBox.headerStyler();
+// 팀 개인 소개 창
+console.log(root.children[1].children);
+styleBox.commonStyler(root.children[1], styleBox.InfoBoxStyleValue);
