295b4965fa3a95d31183fbf571312ec94e3fffb1
jangrubin <wkdfnqls2465@gmail.com>
2023-03-10 15:24:40 +0900

    스타일링 적용중

--- source/modulefile.js
+++ source/modulefile.js
@@ -1,5 +1,7 @@
 const root = document.getElementById("root");
-
+const header = root.children["header"];
+// const main = root.children["main"];
+// const footer = root.children["footer"];
 const _EXAMDATA = {
   teamInformation: {
     teamNumber: 5,
@@ -68,15 +70,17 @@ const _EXAMDATA = {
 //   },
 // };
 // 헤더 스타일링
-innerContent = [];
+// innerContent = [];
 // pageHandler.elementMaker("div", "", "", "root");
 // tagName header로 하면 object형태로 나와서 style 찾기가 난해함
+
 const pageMaker = {
   tagName: ["div"],
-  innerContent: ["하"],
+  innerContent: ["생성"],
   Value: ["id"],
   valueName: ["header", "main", "footer"],
   parent: [root],
+
   // 헤더, 메인, 푸터
   bodyWrap: function () {
     for (let i = 0; i < pageMaker.valueName.length; i++) {
@@ -86,17 +90,59 @@ const pageMaker = {
       this.parent[0].appendChild(element);
     }
   },
-
-  // styler: function (StyleObject) {
-  //   for (let i = 0; i < styleBox.length; i++) {
-  //     this.StyleObject.style[styleBox[i]] = styleSelect[i];
+  // teamInfo: function () {
+  //   for (
+  //     let i = 0;
+  //     i < _EXAMDATA["teamInformation"].teamAllMember.length;
+  //     i++
+  //   ) {
+  //     let element = document.createElement("div");
+  //     // element.textContent = this.innerContent[0];
+  //     // element.setAttribute(this.Value[0], this.valueName[0]);
+  //     root.children[1].appendChild(element);
   //   }
+  //   // console.log("hi");
   // },
+  // 디비전 생성 -> where에 어디의 자식으로 할건지 경로지정, a에 몇개 만들건지 씀
+  div: function (where, a) {
+    for (let i = 0; i < a; i++) {
+      let ele = document.createElement("div");
+      ele.textContent = this.innerContent[0];
+      where.appendChild(ele);
+    }
+  },
 };
-// 생성
-pageMaker.bodyWrap();
 
+// styler: function (StyleObject) {
+//   for (let i = 0; i < styleBox.length; i++) {
+//     this.StyleObject.style[styleBox[i]] = styleSelect[i];
+//   }
+// },
+
+// 태그 생성 영역
+// pageMaker.createDiv(5);
+
+// 헤더 메인 푸터 생성
+pageMaker.bodyWrap();
+// header에 디비전 생성
+pageMaker.div(root.children[0], 1);
 // pageMaker.styler("root");
+// 헤더 자식으로 div 1개 생성
+// pageMaker.teamInfo(1);
+// 메인
+pageMaker.div(root.children[1], 3);
+
+// 푸터
+
+function teamIntro() {
+  for (let i = 0; i < a; i++) {}
+}
+const teamIntroduce = document.createElement("div");
+teamIntroduce.textContent = _EXAMDATA["teamInformation"].teamName;
+root.children[0].appendChild(teamIntroduce);
+// 랜덤 팀원 이름
+// console.dir(_EXAMDATA["teamInformation"].teamAllMember[i].name);
+console.dir(_EXAMDATA["teamInformation"].teamAllMember[0].teamPosition);
 
 // root.style[styleBox[0]] = styleSelect[0];
 // root.style[styleBox[1]] = styleSelect[1];
@@ -110,16 +156,63 @@ pageMaker.bodyWrap();
 
 // }
 
-const header = document.getElementById("header");
 // console.dir(header);
 // header.style.width = "100px";
 
-// headerStyling();
-function elementStyling(StyleObject) {
-  for (let i = 0; i < styleSelect.length; i++) {
-    StyleObject.style[styleSelect[i]] = styleValue[i];
-  }
-}
-const styleSelect = ["width", "height", "backgroundColor"];
-const styleValue = ["100vw", "100vh", "green"];
-elementStyling(root);
+// 헤더 스타일링
+// function elementStyling(StyleObject) {
+//   for (let i = 0; i < styleSelect.length; i++) {
+//     StyleObject.style[styleSelect[i]] = styleValue[i];
+//   }
+// }
+// const styleSelect = ["width", "height", "backgroundColor"];
+
+// const styleValue = ["100vw", "100vh", "green", "red"];
+// elementStyling(root);
+
+// function styleMaker(style) {
+//   Object.keys(style).forEach(function(key) {
+//     root[key] = style[key];
+//   })
+// }
+// console.log(root.style);
+
+// const styleWorld = {
+//   Size: ["width"],
+//   Font: ["fontSize"],
+//   Display: ["display"],
+//   Color: ["color"],
+// };
+
+// // console.dir(styleWorld.Color[0]);
+// // root.style[styleWorld.Color[0]] = styleValue[3];
+
+const styleBox = {
+  // 각 스타일별로 나누기엔 너무 어려움 ㅠ
+  StyleSelect: [
+    // size
+    "width",
+    "height",
+    // color
+    "backgroundColor",
+    "color",
+    "fontSize",
+    "fontFamilly",
+    // display
+    "display",
+    "flexDirection",
+    "justifyContent",
+    "alignItems",
+  ],
+  // 스타일 적용할 변수 선언하고 데이터 넣어주면 됨
+  headerStyleValue: ["100vw", "10vh", "green", "yellow"],
+  mainStyleValue: ["100vw", "70vh", "yellow"],
+  footerStyleValue: [,],
+  styler: function (styleWhere) {
+    for (let i = 0; i < this.StyleSelect.length; i++) {
+      styleWhere.style[this.StyleSelect[i]] = this.headerStyleValue[i];
+    }
+  },
+};
+// console.log(root.children[0].style["width"]);
+styleBox.styler(root.children[0]);
